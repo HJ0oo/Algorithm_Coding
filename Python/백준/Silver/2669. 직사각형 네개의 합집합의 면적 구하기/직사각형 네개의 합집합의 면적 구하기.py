11eeb52150795d65fdbb066e104ee3b4497de63a
HJ0oo <ihjkong@naver.com>
2024-08-03 22:34:03 +0900

    [Silver V] Title: 직사각형 네개의 합집합의 면적 구하기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.py
+++ Python/백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.py
@@ -3,18 +3,19 @@ left_2_x,left_2_y,right_2_x,right_2_y = map(int, input().split())
 left_3_x,left_3_y,right_3_x,right_3_y = map(int, input().split())
 left_4_x,left_4_y,right_4_x,right_4_y = map(int, input().split())
 
-
-on_list=[]
+cnt = 0
+on_list=[[0] * 101 for _ in range(101)]
 def rect(left_x,left_y,right_x,right_y):
+    global cnt
     for i in range(left_x,right_x):
         for j in range(left_y,right_y):
-            on_list.append((i,j))
+            if on_list[i][j] == 0 :
+                on_list[i][j] = 1
+                cnt += 1
 
 rect(left_1_x,left_1_y,right_1_x,right_1_y)
 rect(left_2_x,left_2_y,right_2_x,right_2_y)
 rect(left_3_x,left_3_y,right_3_x,right_3_y)
 rect(left_4_x,left_4_y,right_4_x,right_4_y)
 
-final_on_list=set(on_list)
-
-print(len(final_on_list))
+print(cnt)
