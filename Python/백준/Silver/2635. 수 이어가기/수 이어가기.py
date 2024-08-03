3a90b2f4c5ef50ed35d089938a9b7e5286eb63ef
HJ0oo <ihjkong@naver.com>
2024-08-03 22:35:08 +0900

    [Silver V] Title: 수 이어가기, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Silver/2635. 수 이어가기/수 이어가기.py
+++ Python/백준/Silver/2635. 수 이어가기/수 이어가기.py
@@ -1,36 +1,19 @@
-# 2635
-first_num = int(input())                #입력값이 1일 때
-ans_list=[]                             #ans_list=[]
-num_list=[]                             #num_list=[]
+N = int(input()) # 1~30,000 중 양의 정수
+max_num = 0
 
-def minus(i,j):
-    if((i-j)>=0):
-        num_list.append(i-j)
-        return minus (j,i-j)
+for i in range(1,N+1):
+    ans = [N]
+    n_1 = N
+    next_n = i
+    while next_n >= 0:
+        ans.append(next_n)
+        n_1 , next_n = next_n, n_1
+        next_n -= n_1
 
-
-for sec_num in range (1,first_num+1):   #1
-    num_list=[first_num]                #num_list=[1]
-    num_list.append(sec_num)            #num_list=[1,1]
-    minus(first_num,sec_num)            # .. num_list=[1,1,0,1]
-    ans_list.append(num_list)           #ans_list=[[1,1,0,1]]
-
-
-max_len = 0                             #max_len=0
+    if len(ans) > max_num :
+        max_num = len(ans)
+        final_ans = ans
 
 
-
-if(len(ans_list)!=1):
-    for i in range(len(ans_list)-1):        #range(0,0)
-        if max_len < len(ans_list[i]):
-            max_len = len(ans_list[i])
-            ans = i
-else:                                       #input이 1일 때 오류 해결; len(ans_list)=1 일 때.
-    max_len=len(ans_list[0])
-    ans=0
-
-
-print(max_len)
-
-for i in ans_list[ans]:
-    print(i,end=' ')
+print(max_num)
+print(*final_ans)
