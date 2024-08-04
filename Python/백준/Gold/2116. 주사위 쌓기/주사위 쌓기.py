267e18c8d7512f35a416acb188f03a0c9c184f6c
HJ0oo <ihjkong@naver.com>
2024-08-04 13:12:53 +0900

    [Gold V] Title: 주사위 쌓기, Time: 364 ms, Memory: 32140 KB -BaekjoonHub

--- Python/백준/Gold/2116. 주사위 쌓기/주사위 쌓기.py
+++ Python/백준/Gold/2116. 주사위 쌓기/주사위 쌓기.py
@@ -4,45 +4,36 @@ for _ in range(N):
     input_list = list(map(int,input().split()))
     one_of_dice = [input_list[0],input_list[5],input_list[1],input_list[3],input_list[2],input_list[4]]
     dice.append(one_of_dice)
-    
-# print(dice)
+
 
 # 결국 '하나의 dice에 남아있는 값 중 가장 큰 값' 들의 '합'이 가장 커야하는 것. 전체적인 합을 구하는 것은 의미가 없음
-ans_list = [] # 6개의 경우의 수가 나올 것
+ans_list = [] # 첫번째 주사위에서 고르는 수에 따라 6개의 경우의 수가 나올 것
 
 for start_index in range(6) :
     ans = 0
-    remain = [1,2,3,4,5,6]
+    remain = [1,2,3,4,5,6]                # 각 dice 에서 맨위와 맨아래를 remove로 지운 후,
     remain.remove(dice[0][start_index])
-    ################## sum_of_updown = 0
-    ################## sum_of_updown += dice[0][start_index] #print(dice[0][start_index],"start")
 
-    if start_index % 2 == 0 :   # 0 2 4
+    if start_index % 2 == 0 :     # 0 2 4
         next_num = dice[0][start_index+1]
-    else :                  # 1 3 5
+    else :                        # 1 3 5
         next_num = dice[0][start_index-1]
     remain.remove(next_num)
-    ans += max(remain)
-    ################## sum_of_updown += next_num #print(next_num,"start")
+    ans += max(remain)                    # 남은 4개의 수 중 max 값을 ans에 더함
 
     for dice_num in range(1,N) :
         for i in range(6):
-            # print(dice_num,"dice_num")
             if dice[dice_num][i] == next_num :
                 remain = [1,2,3,4,5,6]
                 remain.remove(next_num)
-                ################## sum_of_updown += next_num #print(i,next_num,"find same thing")
                 if i % 2 == 0 : next_num = dice[dice_num][i+1]
                 else : next_num = dice[dice_num][i-1]
                 remain.remove(next_num)
-                ################## sum_of_updown += next_num #print(dice_num,next_num,"next_num")
                 ans += max(remain)
                 break
-
-    # print("ennd")
-    # print(ans)
-    ans_list.append(ans)
-print(max(ans_list))
+                
+    ans_list.append(ans)                # N개의 주사위에 대해 모두 수행한 후의 ans 값
+print(max(ans_list))                    # 6개의 경우의 수 중 max 값 출력
 
 
 
