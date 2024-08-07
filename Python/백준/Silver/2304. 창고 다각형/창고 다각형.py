4d7e8468570e735a9a5d3a24ee0c893b5a129fc5
HJ0oo <ihjkong@naver.com>
2024-08-07 16:53:01 +0900

    [Silver II] Title: 창고 다각형, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Silver/2304. 창고 다각형/창고 다각형.py
+++ Python/백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -1,13 +1,12 @@
+# 더 생각해볼 코드
 # N = int(input())
 # total_list = []
 # H_list = []
 # for _ in range(N) :
 #     L, H = map(int,input().split())
 #     total_list.append([L,H])
-#
 # print(total_list)
 # total_list.sort() # 첫 번째 값 기준으로 sort되나봥
-#
 # print(total_list)
 
 N = int(input())
@@ -15,35 +14,24 @@ total_list = [0] * 1001
 for _ in range(N):
     L, H = map(int, input().split())
     total_list[L] = H
-#
-# print(total_list)
-# print(max(total_list))
-# print(total_list.index(max(total_list)))
 
-standard_index = total_list.index(max(total_list))
-# for m in range(standard_index-1,-1,-1): # standard_index-1 ~ 0
-#     pass
-# for m in range(standard_index,-1,-1): # standard_index+1 ~ 0
-#     pass
+standard_index = total_list.index(max(total_list)) # 제일 높은 거 기준으로 삼고
 
 now_max = 0
-for i in range(0,standard_index) :
-    if now_max <= total_list[i] :
-        now_max = total_list[i]
-        total_list[i] = now_max
-    else :
-        total_list[i] = now_max
-# print(total_list)
-# print("ddd")
-# print(len(total_list))
-
-now_max = 0
-for i in range(1000,standard_index,-1) :
+for i in range(0,standard_index) :          # 기준 왼쪽 부분 - 0 ~ standard_index-1 까지
     if now_max <= total_list[i] :
         now_max = total_list[i]
         total_list[i] = now_max
     else :
         total_list[i] = now_max
 
-# print(total_list)
+now_max = 0
+for i in range(1000,standard_index,-1) :    # 기준 오른쪽 - 1000 ~ standard_index+1 까지
+    if now_max <= total_list[i] :
+        now_max = total_list[i]
+        total_list[i] = now_max
+    else :
+        total_list[i] = now_max
+
+# print(total_list) # [0, 0, 4, 4, 6, 6, 6, 6, 10, 8, 8, 8, 8, 8, 8, 8, 0, 0, 0, 0, 0, 0, 0, ...]
 print(sum(total_list))
