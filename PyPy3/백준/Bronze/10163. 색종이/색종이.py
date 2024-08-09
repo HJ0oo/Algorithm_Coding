a377257bb1fe1d8f124956f0bced545157a9582a
HJ0oo <ihjkong@naver.com>
2024-08-09 09:39:46 +0900

    [Bronze I] Title: 색종이, Time: 408 ms, Memory: 118232 KB, Score: 100 point -BaekjoonHub

--- PyPy3/백준/Bronze/10163. 색종이/색종이.py
+++ PyPy3/백준/Bronze/10163. 색종이/색종이.py
@@ -1,18 +1,30 @@
-# 방법 2 - 시간초과.
-N = int(input())
-final_dic = {}
-final_list = [0] * N # 색종이별 최종 값을 담을 리스트
 
-for k in range(N):
-    left_x, left_y, length, height = map(int, input().split())
+# 방법 1 - 0 행렬 만들어서 직접 색칠
+import sys
+N = int(sys.stdin.readline().rstrip())
+ans_list = [0] * N
+total_arr = [[0] * 1002 for _ in range(1002)]
+
+for paper_num in range(1,N+1):
+    left_x,left_y,length,height = map(int, sys.stdin.readline().rstrip().split())
     right_x = left_x + length
     right_y = left_y + height
+
     for i in range(left_x,right_x):
         for j in range(left_y,right_y):
-            final_dic[(i,j)] = k # 딕셔너리 어차피 key-value 하나니까 뒤에 입력값으로 덮어쓰기
+            total_arr[i][j] = paper_num
 
-for key in final_dic :
-    final_list[final_dic[key]] += 1
+# # 기존 코드 - 색종이 개수 만큼 for문을 더 돌림
+# for paper_num in range(1,N+1) :
+#     for i in range(1002):
+#         for j in range(1002):
+#             if total_arr[i][j] == paper_num :
+#                 ans_list[paper_num-1] += 1
+# 배열 탐색을 한 번만 수행하여 각 색종이의 넓이 계산
+for i in range(1002):
+    for j in range(1002):
+        if total_arr[i][j] != 0:  # 0이 아닌 경우, 즉 색종이가 덮고 있는 경우
+            ans_list[total_arr[i][j] - 1] += 1
 
-for ans in final_list :
-    print(ans)
+for p in ans_list:
+    print(p)
