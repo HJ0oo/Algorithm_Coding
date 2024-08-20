848699de3f6e04bb5ab6177a2d8e451533bd8441
HJ0oo <ihjkong@naver.com>
2024-08-20 15:53:12 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 142 ms, Memory: 48,444 KB -BaekjoonHub

--- Python/SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
+++ Python/SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
@@ -1,13 +1,32 @@
-T = int(input())
-for tc in range(1,T+1):
-    cnt = 1
-    N = int(input())
-    num_list = ['0','1','2','3','4','5','6','7','8','9']
-    while num_list:
-        multi_str = cnt * N
-        for s in str(multi_str):
-            if s in num_list :
-                num_list.remove(s)
-        cnt += 1
+#include <iostream>
+#include <string>
+#include <unordered_set>
+using namespace std;
 
-    print(f"#{tc} {N * (cnt-1)}")
+int main() {
+	int T;
+	cin >> T;
+
+	for (int tc = 1; tc <= T; ++tc) {
+		int N;
+		cin >> N;
+
+		unordered_set<char> num_set{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+		int cnt = 1;
+
+		while (!num_set.empty()) {
+			int multi_num = cnt * N;
+			string multi_str = to_string(multi_num);
+
+			for (char c : multi_str) {
+				num_set.erase(c);
+			}
+
+			++cnt;
+		}
+
+		cout << "#" << tc << " " << N * (cnt - 1) << endl;
+	}
+
+	return 0;
+}
