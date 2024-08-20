de7b84df0faaa8dcfebb76b493547e453a469503
HJ0oo <ihjkong@naver.com>
2024-08-20 17:08:22 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 123 ms, Memory: 46,836 KB -BaekjoonHub

--- Python/SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
+++ Python/SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
@@ -1,32 +1,12 @@
-#include <iostream>
-#include <string>
-#include <unordered_set>
-using namespace std;
+T = int(input())
+for tc in range(1,T+1):
+    cnt = 1
+    N = int(input())
+    check = 0                   # 0 ~ 9 까지의 모든 숫자 - 비트마스크 0000000000 이라고 생각.
+    multi = N
+    while check < 2**10 - 1 :   # 1111111111 이 될 때까지. = 2**N - 1
+        for s in str(multi) :
+            check = check | (1<<int(s))
+        multi += N
 
-int main() {
-	int T;
-	cin >> T;
-
-	for (int tc = 1; tc <= T; ++tc) {
-		int N;
-		cin >> N;
-
-		unordered_set<char> num_set{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-		int cnt = 1;
-
-		while (!num_set.empty()) {
-			int multi_num = cnt * N;
-			string multi_str = to_string(multi_num);
-
-			for (char c : multi_str) {
-				num_set.erase(c);
-			}
-
-			++cnt;
-		}
-
-		cout << "#" << tc << " " << N * (cnt - 1) << endl;
-	}
-
-	return 0;
-}
+    print(f"#{tc} {multi - N}")
