a6fe7ee8d5a3558d30d035f9819c444d5b8f45f7
HJ0oo <ihjkong@naver.com>
2024-08-20 15:53:48 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 19 ms, Memory: 13,564 KB -BaekjoonHub

---
+++ C++/SWEA/D2/1288. 새로운 불면증 치료법/README.md
@@ -0,0 +1,15 @@
+# [D2] 새로운 불면증 치료법 - 1288 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV18_yw6I9MCFAZN) 
+
+### 성능 요약
+
+메모리: 13,564 KB, 시간: 19 ms, 코드길이: 519 Bytes
+
+### 제출 일자
+
+2024-08-20 15:53
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ C++/SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+	for (int tc = 1; tc <= T; ++tc) {
+		int N;
+		cin >> N;
+
+		unordered_set<char> num_set{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+		int cnt = 1;
+
+		while (!num_set.empty()) {
+			int multi_num = cnt * N;
+			string multi_str = to_string(multi_num);
+
+			for (char c : multi_str) {
+				num_set.erase(c);
+			}
+
+			++cnt;
+		}
+
+		cout << "#" << tc << " " << N * (cnt - 1) << endl;
+	}
+
+	return 0;
+}
