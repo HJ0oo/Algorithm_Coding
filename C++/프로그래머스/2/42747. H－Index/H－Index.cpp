a1c48b17d244a051c8693e839d0bdb3bb35be514
HJ0oo <ihjkong@naver.com>
2024-10-09 18:45:24 +0900

    [level 2] Title: H-Index, Time: 0.04 ms, Memory: 4.18 MB -BaekjoonHub

--- C++/프로그래머스/2/42747. H－Index/H－Index.cpp
+++ C++/프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -1,24 +1,17 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int solution(vector<int> citations) {
-    // 어떤 과학자가 발표한 논문의 인용 횟수를 담은 배열 citations가 매개변수로 주어질 때,
-    // 이 과학자의 H-Index를 return 하도록 solution 함수를 작성해주세요.
-    int arr[10001] = {} ;
-    int n = citations.size() ;
-    for (int i=0;i<n;i++){
-        arr[citations[i]] += 1;
-    }
-    
-    int sum = 0;
-    for (int i=10000;i>=0;i--){
-        sum += arr[i];
-        if (sum>=i){
+    sort(citations.begin(), citations.end(), greater<int>());
+
+    for (int i = 0; i < citations.size(); ++i) {
+        if (citations[i] < i + 1) {
             return i;
         }
     }
-    return -99999999;
 
+    return citations.size();
 }
--- C++/프로그래머스/2/42747. H－Index/README.md
+++ C++/프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747#) 
 
 ### 성능 요약
 
-메모리: 4.15 MB, 시간: 0.05 ms
+메모리: 4.18 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 09일 14:39:47
+2024년 10월 09일 18:45:21
 
 ### 문제 설명
 
