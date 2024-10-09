6b369701b594032ca85faaaec9ac261f32354913
HJ0oo <ihjkong@naver.com>
2024-10-09 20:28:51 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 4.12 MB -BaekjoonHub

--- C++/프로그래머스/2/42747. H－Index/H－Index.cpp
+++ C++/프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -7,9 +7,10 @@ using namespace std;
 int solution(vector<int> citations) {
     sort(citations.begin(), citations.end(), greater<int>());
 
-    for (int i = 0; i < citations.size(); ++i) {
+    for (int i = 0; i < citations.size(); i++) {
         if (citations[i] < i + 1) {
             return i;
+            
         }
     }
 
