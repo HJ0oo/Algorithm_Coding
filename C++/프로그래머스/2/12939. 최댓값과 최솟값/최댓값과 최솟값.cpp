209c63d409779a92ddc7cb699c1b71b28d1bd45a
HJ0oo <ihjkong@naver.com>
2024-10-17 15:49:58 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 4.23 MB -BaekjoonHub

--- C++/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ C++/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -13,6 +13,8 @@
 */
 using namespace std;
 
+/*
+// 시도 1
 string solution(string s) {
     stringstream string_into_stream(s);
     vector<int> numbers;
@@ -25,7 +27,35 @@ string solution(string s) {
     sort(numbers.begin(),numbers.end());
     
     string answer = to_string(*numbers.begin()) + " " + to_string(*(numbers.end()-1));
-    // answer += numbers.back();
+                                                                // numbers.back();
     
+    return answer;
+}
+*/
+
+
+// 시도 2
+string solution(string s) {
+    string sTemp = "";
+    vector<int> my_vec;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == ' ')
+        {
+            my_vec.push_back(stoi(sTemp)); // 음수인 경우에도 -까지 담은 그 string을 int로 바꿔줄거니까.
+            sTemp = ""; // 초기화 잊지 않기
+            continue;
+        }
+
+        sTemp += s[i];
+    }
+
+    my_vec.push_back(stoi(sTemp));
+
+    sort(my_vec.begin(), my_vec.end());
+
+    string answer = to_string(*my_vec.begin()) + " " + to_string(*(my_vec.end()-1));
+
     return answer;
 }
