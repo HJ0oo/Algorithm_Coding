62cfe79cedb620575295e90df17b1e9331ee0a82
HJ0oo <ihjkong@naver.com>
2024-10-20 21:20:21 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- C++/프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
+++ C++/프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -1,3 +1,31 @@
+/*
+isupper, islower, toupper, tolower 등의 함수는 C 표준 라이브러리의 <ctype.h>에 정의되며, C++에서는 <cctype>로 제공됩니다. 이 함수들은 문자를 대문자나 소문자로 변환하거나, 문자가 대문자인지 소문자인지 확인하는 데 사용됩니다
+다만, <string>만 포함해도 작동하는 이유는 일부 컴파일러가 자동으로 <cctype> 또는 <ctype.h>를 포함하거나 간접적인 의존성을 가질 수 있기 때문입니다. 하지만 표준적으로는 <cctype>를 명시적으로 포함하는 것이 권장됩니다
+*/
+#include <string>
+#include <vector>
+// #include <cctype>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    answer += toupper(s[0]);
+    for (int i = 1; i < s.size(); i++)
+        if (s[i - 1] == ' '){
+            answer += toupper(s[i]);
+        }
+        else {
+            answer += tolower(s[i]);
+        }
+
+    return answer;
+}
+
+
+/////////////////////////////
+/*
+
 #include <string>
 #include <vector>
 
@@ -32,3 +60,4 @@ string solution(string s) {
     }
     return answer;
 }
+*/
